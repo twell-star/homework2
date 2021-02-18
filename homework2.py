dffd8306d45f142f37491a07d40fa6f4e9711ccc
twell-star <novikov.igor@mail.ru>
2021-02-18 18:23:32 +0300

    Version 1.1

--- homework2.py
+++ homework2.py
@@ -7,3 +7,10 @@ account2 = {'login': 'petr', 'password': 'q2'}
 account3 = {'login': 'olga', 'password': 'q3'}
 account4 = {'login': 'anna', 'password': 'q4'}
 
+# создаю переменные типа 'словарь' со структурой 'user'
+
+user1 = {'name': 'Иван', 'age': 20, 'account': account1}
+user2 = {'name': 'Петр', 'age': 25, 'account': account2}
+user3 = {'name': 'Ольга', 'age': 18, 'account': account3}
+user4 = {'name': 'Анна', 'age': 27, 'account': account4}
+
