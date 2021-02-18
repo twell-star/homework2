d66b58d359f874edf0ef630a63fd8de59dba0c45
twell-star <novikov.igor@mail.ru>
2021-02-18 18:15:22 +0300

    Version 1

--- homework2.py
+++ homework2.py
@@ -1,5 +1,7 @@
 # домашнее задание по Модулю №2
 
+# создаю переменные типа 'словарь' со структурой 'account'
+
 account1 = {'login': 'ivan', 'password': 'q1'}
 account2 = {'login': 'petr', 'password': 'q2'}
 account3 = {'login': 'olga', 'password': 'q3'}
