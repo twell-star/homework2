034b586900df0e878da25b4b752ef29a727dc23a
twell-star <novikov.igor@mail.ru>
2021-02-18 19:06:46 +0300

    Version 1.2

--- homework2.py
+++ homework2.py
@@ -14,3 +14,15 @@ user2 = {'name': 'Петр', 'age': 25, 'account': account2}
 user3 = {'name': 'Ольга', 'age': 18, 'account': account3}
 user4 = {'name': 'Анна', 'age': 27, 'account': account4}
 
+# создаю список user_list
+
+user_list = [user1, user2, user3, user4]
+
+# запрос ввода имени ключа
+
+key_name = input('Введите ключ (name или account):')
+
+# преобразование имени ключа в строчные буквы
+
+key_name = key_name.lower()
+
