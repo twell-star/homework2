47fe3e9fe18119ab6abddba3a5a37cd1c25ebdab
twell-star <novikov.igor@mail.ru>
2021-02-18 18:14:15 +0300

    Version 1

--- homework2.py
+++ homework2.py
@@ -1,1 +1,7 @@
 # домашнее задание по Модулю №2
+
+account1 = {'login': 'ivan', 'password': 'q1'}
+account2 = {'login': 'petr', 'password': 'q2'}
+account3 = {'login': 'olga', 'password': 'q3'}
+account4 = {'login': 'anna', 'password': 'q4'}
+
