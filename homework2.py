bad89c057fdb224a28abe6747b43c0c498d44114
twell-star <novikov.igor@mail.ru>
2021-02-19 10:35:04 +0300

    Version 1.6

--- homework2.py
+++ homework2.py
@@ -1,4 +1,4 @@
-# домашнее задание по Модулю №2
+# домашнее задание по Модулю #2
 
 # создаю переменные типа 'словарь' со структурой 'account'
 
@@ -27,12 +27,18 @@ key_name = input('Введите ключ (name или account):')
 key_name = key_name.lower()
 
 # проверка имени ключа через словарь 'user1'
+# не хотел привязываться жестко к конкретному словарю, но более изящного решения не нашел
 
 try:
     control_key_name = user1[key_name]
 except KeyError:
     print('Введенный ключ не найден')
 
-print(f"значение ключа {key_name} для юзера 1 = {user1.get(key_name)}")
+# вывод значений ключа для всех юзеров через операцию цикла
+# хотя операции циклов мы как бы еще не проходили, но не хотелось тупо принтовать каждую строку
 
+counter_user = 0  # определяю счетчик юзеров
 
+while counter_user < len(user_list):
+    counter_user = counter_user + 1
+    print(f"значение ключа {key_name} для юзера {counter_user} = {user_list[counter_user-1].get(key_name)}")
